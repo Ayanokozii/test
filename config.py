874c3c375e1b9b3ea7252c17d5acac6b864ffbd1
Ayanokozii <ayanokozi0925@gmail.com>
2024-07-21 16:14:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     # Pyro client config
     API_ID = os.getenv("API_ID", "21189715")  # ⚠️ Required
     API_HASH = os.getenv("API_HASH", "988a9111105fd2f0c5e21c2c2449edfd")  # ⚠️ Required
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "7044609105:AAEbL7gi84MMfESHFnsoSBsHVVyHNB1IQCo")  # ⚠️ Required
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7151676790:AAFIjgmsLZJsdDJYRWmQFaZGGGAxX3Tt3Cg")  # ⚠️ Required
 
     # Database config
     DB_URL = os.getenv("DB_URL", "mongodb+srv://ayanosuvii0925:subhichiku123@cluster0.uw8yxkl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # ⚠️ Required
@@ -25,7 +25,7 @@ class Config:
     DEFAULT_LEAVE_MSG = os.getenv("LEAVE_MSG", "By {user},\nSee You Again 👋\n\nFrom **{title}**")
 
     # User client config
-    SESSION = os.getenv("SESSION", "BQGpTXQAWQj7skbUYWJG68v_k91mlNI87pQCLM-fbIwpJy7SHfkOvUjTUumhYW-kJpgmf9JLY68qg_IAp_i5XQXI4cB5FNy_vYyc5VcAnzt7sze0TC5yh-4dPHtwXAMDdAlt2ZemBCGow9J4DXsQ5d0JcxdOARLqtsW8u_d__0GSVPpLSFftf179T6KzWCHRTYOfx7NmsSyysg3MuCP0j2jT0iFXSmoS_4mHJ49JLaFxB-Ln2-rHh-Sovd_baZ3-6F_c-YG7GhfdLyn7iHKrn5SeTskmczV4geih58cD9RBcxykpKkbpvcXBV8XYfSuCKZF3zKpe-2qY59QHmhTO6ikkTW_jEgAAAAGruX5VAA")  # ⚠️ Required
+    SESSION = os.getenv("SESSION", "BQFPq6kAp2lpFF7n8_jZ4b4ZMM8x7_8_HLIqjFHy4tDZxln9d90h1m7a4u60_K2032J-Fh1Gi-T5u3jd4mmHiJ1EO05SpgmDr5kwTgFMq2E3QQ0Jg_AmYwhed8UBBjNWwas7riu2U4YiOTu_K5_3RynzLhuu98D-cqzuEhLDgRaid7JUHi3IitCRLig-4WS36w29Z_Ak1KoKlQpujqOHNfE0RsZxnHzt0u1Th-pn_1u3Mon-pMWu-nr-8JdfDdy72B5kB8pgYkrDtdvbO2pcUSEn6wmNCeurSRmwQVclwcL3KA9LJUDWy6pgMafye72t0Up9nDkkVbajVdlkwPPqTjZPmoaImwAAAAGd_UK4AA")  # ⚠️ Required
 
     # Webhook configuration
     WEBHOOK = bool(os.getenv("WEBHOOK", True))
